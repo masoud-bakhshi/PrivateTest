c5fa4b249acc6dbdc9ff2ea1443f33b03a8f75a9
Masoud Bakhshi <masoud.main@gmail.com>
2021-09-21 12:48:52 +0430

    basename

--- App.js
+++ App.js
@@ -4,7 +4,10 @@ import My404Component from "./pages/my404Component";
 import { BrowserRouter, Route, Switch, Redirect } from "react-router-dom";
 function App() {
   return (
+   
     //basename="/5002/"
+	 //basename="/5002/"
+	  //basename="/5002/"
     <BrowserRouter basename="/6092b28a8fda4729541cf5a5/">
       <Switch>
         <Route path="/" component={Home} exact />
