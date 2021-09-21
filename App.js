c31ec5ac56346179321c3462adf6def775857197
Masoud Bakhshi <masoud.main@gmail.com>
2021-09-21 12:54:20 +0430

    new 366666666

--- App.js
+++ App.js
@@ -9,6 +9,8 @@ function App() {
 	 //basename="/5002/"
 	  //basename="/5002/"
     <BrowserRouter basename="/6092b28a8fda4729541cf5a5/">
+	  <BrowserRouter basename="/6092b28a8fda4729541cf5a5/">
+	    <BrowserRouter basename="/6092b28a8fda4729541cf5a5/">
       <Switch>
         <Route path="/" component={Home} exact />
   <Route path="/" component={Home} exact />
